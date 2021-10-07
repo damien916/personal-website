5a693f6ff7e253d9818ccc314f4b332153c672ed
Damien Virlouvet <damienv@swapcard.com>
2021-10-07 21:46:20 +0200

    Intro

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,26 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   padding: 48px;
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  font-size: 39px;
+`;
+
+const SubTitle = styled.p`
+  font-size: 21px;
 `;
 
 export default function App() {
-  return <Wrapper>Hello world</Wrapper>;
+  return (
+    <Wrapper>
+      <Title>👋 Hi there! I’m Damien,</Title>
+      <SubTitle>
+        I’m a frontend developer and I’m used to work with React, Typescript,
+        GraphQL, styled-components. It sounds like a nice stack, isn’t it?
+      </SubTitle>
+    </Wrapper>
+  );
 }
