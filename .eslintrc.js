62a30de921615e7a48634088cef8313ba1087c77
Damien Virlouvet <damienv@swapcard.com>
2021-10-07 21:34:22 +0200

    First commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,27 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['react', '@typescript-eslint', 'prettier'],
-    extends: [
-        'airbnb-typescript',
-        'airbnb/hooks',
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'prettier/react',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
-    ],
-    env: {
-        browser: true,
-        jasmine: true,
-        jest: true,
-        node: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    // Airbnb's ESLint config requires this
-    parserOptions: {
-        project: './tsconfig.json',
-    },
-    rules: {
-        // Include .prettierrc.js rules
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-        // We will use TypeScript's types for component props instead
-        'react/prop-types': 'off',
-        // We don't want unused vars
-        '@typescript-eslint/no-unused-vars': ['error'],
-    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-const App: React.FC = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-};
+const Wrapper = styled.div`
+  padding: 48px;
+`;
 
-export default App;
+export default function App() {
+  return <Wrapper>Hello world</Wrapper>;
+}
